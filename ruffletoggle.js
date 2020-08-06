8341d6fecfb6f42c16af3a37193669c0a311a4ce
Tsuki Superior <tsukisuperior@gmail.com>
2020-08-06 16:42:57 -0500

    Update ruffletoggle.js

--- ruffletoggle.js
+++ ruffletoggle.js
@@ -1,5 +1,8 @@
 var element = document.createElement("embed");
 element.src = localStorage.getItem("game");
+element.width = "640";
+element.width = "480";
+
 document.body.appendChild(element);
 
 if (localStorage.getItem("useRuffle") == "true") {
