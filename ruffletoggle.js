05329b1719f8841ce8b99625504a64083da2706e
Tsuki Superior <tsukisuperior@gmail.com>
2020-08-06 16:36:40 -0500

    dc

--- ruffletoggle.js
+++ ruffletoggle.js
@@ -1,5 +1,8 @@
-if (localStorage.getItem("useRuffle") == "true"){
-    var element= document.createElement("script");
-    element.src="./ruffle/ruffle.js";
+if (localStorage.getItem("useRuffle") == "true") {
+    var element = document.createElement("embed");
+    element.src = localStorage.getItem("game");
+    document.body.appendChild(element);
+    element = document.createElement("script");
+    element.src = "./ruffle/ruffle.js";
     document.body.appendChild(element);
 }
