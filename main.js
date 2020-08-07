427a49f64c7b3a2413efe62e973fea11a788c367
Tsuki Superior <tsukisuperior@gmail.com>
2020-08-06 20:22:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-var element = document.createElement("embed");
-element.src = localStorage.getItem("game");
-element.width = "1280px";
-element.height = "1024px";
-document.body.appendChild(element);
-
-if (localStorage.getItem("useRuffle") == "true") {
-    element = document.createElement("script");
-    element.src = "./ruffle/ruffle.js";
-    document.body.appendChild(element);
-}
 
 var baseURL = "https://raw.githack.com/tsukisuperior/flash-games/master/",           //The base url for the repo
     ruffleToggleButton = document.getElementById("ruffleToggle"),                    //The message element at the top of the page
