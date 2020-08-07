50ba909f41fe47e4300e653de18fe391fb6f5797
Tsuki Superior <tsukisuperior@gmail.com>
2020-08-07 09:57:23 -0500

    password protect

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var baseURL = "https://raw.githack.com/tsukisuperior/flash-games/master/",      
     ruffleToggleButton = document.getElementById("ruffleToggle"),                    //The message element at the top of the page
     files = [],                                                                      //the list of file names
     list = document.getElementById("list"),                                          //file listing dropdown box
-    exceptedFiles = /\.git*/;                                                        //Regular Expression to find .gitingore and the such
+    exceptedFiles = /\.git*/,                                                         //Regular Expression to find .gitingore and the such
+    swfInfo = {};
+
 
 function getFileListing(target) {                                                    //gets file listing from github repo
     var xhttp = new XMLHttpRequest();
@@ -37,12 +39,44 @@ function getURL() {                                                             
     localStorage.setItem("game", baseURL + list.value)
     location.reload();
 }
+
 function toggleRuffle() {                                                                       // save the state of ruffles activation and reload
     localStorage.setItem("useRuffle", localStorage.getItem("useRuffle") != "true");
     location.reload();
 }
+
+function getInfofile() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            swfInfo = JSON.parse(this.responseText);
+        }
+    };
+    xhttp.open("GET", target, true);
+    xhttp.send();
+}
+function startSwf() {
+    var dimensions;
+    if (typeof swfInfo[name] != "undefined" && typeof swfInfo[name]["Dimensions"] == "string" && (swfInfo[name]["Dimensions"].toLowerCase() != "unknown" || swfInfo[name]["Dimensions"].toLowerCase() != "")) {
+        dimensions = swfInfo[name]["Dimensions"].toLowerCase().split("x");
+
+    } else {
+        dimensions = [1280, 1024];
+    }
+    var element = document.createElement("embed");
+    element.src = localStorage.getItem("game");
+    element.width = dimensions[1] + "px";
+    element.height = dimensions[2] + "px";
+    document.body.appendChild(element);
+    if (localStorage.getItem("useRuffle") == "true") {
+        element = document.createElement("script");
+        element.src = "./ruffle/ruffle.js";
+        document.body.appendChild(element);
+    }
+}
+
 list.addEventListener("change", getURL);
 ruffleToggleButton.addEventListener("click", toggleRuffle);
 getFileListing("https://api.github.com/repos/tsukisuperior/flash-games/contents");            //get the listing
-
-
+getInfofile();
+startSwf();
